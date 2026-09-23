730e5f5ea1c8dae55d2e2bd6215fcd6da0606db4
Arpan Sarkar <mynameari789@gmail.com>
2026-09-23 15:12:15 +0530

    Fix camera follow method to update position correctly

--- camera.py
+++ camera.py
@@ -6,7 +6,7 @@ class Camera:
     def __init__(self, width, height, world_width):
         self.rect = pg.Rect(0, 0, width, height)
         self.world_width = world_width
-
+  
     def follow(self, target):
         desired = target.centerx - self.rect.width // 2
         self.rect.x += int((desired - self.rect.x) * 0.12)
